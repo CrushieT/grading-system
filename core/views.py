bd1a2f2dd6d55c63f40b4cb8e1678ff383496bdb
aundraytafalla <aundraytafalla@mabinicolleges.edu.ph>
2026-05-16 23:37:22 +0800

    Update Templates

--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'index.html')
+def login_page(request):
+    return render(request, 'login.html')
+
+def dashboard_page(request):
+    return render(request, "dashboard.html")
